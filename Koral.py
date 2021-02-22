94b43c84c2bec5034e41e5b10a026612b7e1f700
JBL123-cell <55876456+JBL123-cell@users.noreply.github.com>
2021-02-22 22:34:36 +0300

    Add files via upload

---
+++ Koral.py
@@ -0,0 +1,19 @@
+import wikipediaapi as wikiapi
+
+wiki = wikiapi.Wikipedia('ru')
+page = wiki.page('Коралловые рифы')
+
+links = page.links
+doc = []
+for title in sorted(links.keys()):
+    page_1 = wiki.page(title)
+    if page_1.exists():
+        doc.append((page_1.summary))
+with open('doc.json', 'w', encoding='utf-8') as file:
+    for item in doc:
+        file.write("{}\n".format(item))
+
+
+
+
+
